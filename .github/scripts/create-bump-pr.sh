1af19abd06e328e91aea94bc9e849edc750f2806
Andrii Oriekhov <andriyorehov@gmail.com>
2023-08-25 00:24:12 +0300

    try

--- .github/scripts/create-bump-pr.sh
+++ .github/scripts/create-bump-pr.sh
@@ -1,11 +1,11 @@
 read package_name
 cd "packages/${package_name}"
-#git checkout -b "${package_name}-bump"
+git checkout -b "${package_name}-bump"
 npm version patch
-#git config --global user.name 'Your Name'
-#git config --global user.email 'andriyorehov@gmail.com'
-#git add .
-#git commit -m 'bump version'
-#git push origin bump
-#gh pr create --title "Pull request title" --body "Pull request body"
+git config --global user.name 'Your Name'
+git config --global user.email 'andriyorehov@gmail.com'
+git add .
+git commit -m 'bump version'
+git push origin bump
+gh pr create --title "Pull request title" --body "Pull request body"
 
