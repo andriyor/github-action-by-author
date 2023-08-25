e1a7c7ed767b0a8f129e8076f9251f3d53dfbf18
Andrii Oriekhov <andriyorehov@gmail.com>
2023-08-25 12:27:52 +0300

    try

--- .github/scripts/main.sh
+++ .github/scripts/main.sh
@@ -3,5 +3,5 @@ OUTPUT=$(git show --name-only --numstat --format= | ./.github/scripts/changed-pa
 for key in $OUTPUT
 do
   echo "make pr for ${key}"
-  echo ${key} | .github/scripts/create-bump-pr.sh
+#  echo ${key} | .github/scripts/create-bump-pr.sh
 done
