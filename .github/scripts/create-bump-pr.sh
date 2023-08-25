15eefe9767f2e8ac5de124781a95a380574bd714
Andrii Oriekhov <andriyorehov@gmail.com>
2023-08-25 13:42:29 +0300

    changes to qr package

--- .github/scripts/create-bump-pr.sh
+++ .github/scripts/create-bump-pr.sh
@@ -6,6 +6,6 @@ git config --global user.name 'Your Name'
 git config --global user.email 'andriyorehov@gmail.com'
 git add .
 git commit -m 'bump version'
-git push origin bump
-#gh pr create --title "Pull request title" --body "Pull request body"
+git push origin "${package_name}-bump"
+gh pr create --title "Pull request title" --body "Pull request body"
 
