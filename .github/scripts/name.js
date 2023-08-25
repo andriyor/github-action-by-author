2910e9cfe945ddb2710bbf00965047ffe257e144
Andrii Oriekhov <andriyorehov@gmail.com>
2023-08-25 14:47:02 +0300

    add script

--- .github/scripts/name.js
+++ .github/scripts/name.js
@@ -1,16 +1,15 @@
 module.exports = async ({ github, context }) => {
-  const query = `query() {
-      repository(owner: "andriyor", name: "github-action-by-author") {
-        ref(qualifiedName: "main") {
-          target {
-            ... on Commit {
-               history(first: 1, path: "packages/qr/locales/en-US.json") {
-            edges {
-              node {
-                author {
-                  user {
-                    login
-                  }
+  const query = `{
+    repository(owner: "andriyor", name: "github-action-by-author") {
+      ref(qualifiedName: "main") {
+        target {
+          ... on Commit {
+             history(first: 1, path: "packages/qr/locales/en-US.json") {
+          edges {
+            node {
+              author {
+                user {
+                  login
                 }
               }
             }
@@ -19,7 +18,8 @@ module.exports = async ({ github, context }) => {
       }
     }
   }
-            }`;
+}
+  }`;
   const result = await github.graphql(query);
     console.log(result);
     return context.payload.client_payload;
---
+++ main.js
@@ -0,0 +1,61 @@
+const octokit = require("@octokit/graphql");
+
+// const result = octokit.graphql(
+//     `
+//     {
+//       repository(owner: "octokit", name: "graphql.js") {
+//         issues(last: 3) {
+//           edges {
+//             node {
+//               title
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `,
+//     {
+//         headers: {
+//             authorization: `token`,
+//         },
+//     },
+// );
+
+// result.then(res => {
+//     console.log(res.repository);
+// })
+
+const result = octokit.graphql(
+  `
+    {
+      repository(owner: "andriyor", name: "github-action-by-author") {
+        ref(qualifiedName: "main") {
+          target {
+            ... on Commit {
+               history(first: 1, path: "packages/qr/locales/en-US.json") {
+            edges {
+              node {
+                author {
+                  user {
+                    login
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+    }
+  `,
+  {
+    headers: {
+      authorization: `token`,
+    },
+  }
+);
+
+result.then((res) => {
+  console.log(res.repository);
+});
