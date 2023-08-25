15e5297276709b574aa0568bb83e29d4eaf36559
Andrii Oriekhov <andriyorehov@gmail.com>
2023-08-25 16:18:13 +0300

    cleanup

--- .github/scripts/changed-packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pattern="packages/([^/]+)/locales"
-
-while IFS= read -r line; do
-    if [[ $line =~ $pattern ]]; then
-        echo "${BASH_REMATCH[1]}"
-    fi
-done
--- .github/scripts/main.sh
+++
@@ -1,7 +0,0 @@
-OUTPUT=$(git diff-tree --no-commit-id --name-only -r HEAD | ./.github/scripts/changed-packages.sh)
-
-for key in $OUTPUT
-do
-  echo "make pr for ${key}"
-  echo ${key} | .github/scripts/create-bump-pr.sh
-done
