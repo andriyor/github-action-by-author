bf78b5f97cbfbaae9030455b766d42b2dc22a409
Andrii Oriekhov <andriyorehov@gmail.com>
2023-08-25 13:39:52 +0300

    fix

--- .github/scripts/create-bump-pr.sh
+++ .github/scripts/create-bump-pr.sh
@@ -7,5 +7,5 @@ git config --global user.email 'andriyorehov@gmail.com'
 git add .
 git commit -m 'bump version'
 git push origin bump
-gh pr create --title "Pull request title" --body "Pull request body"
+#gh pr create --title "Pull request title" --body "Pull request body"
 
