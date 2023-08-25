dda7d530bbd585612e28803e2653813ea0decbff
Andrii Oriekhov <andriyorehov@gmail.com>
2023-08-25 14:31:08 +0300

    add script

--- .github/scripts/name.js
+++ .github/scripts/name.js
@@ -1,3 +1,26 @@
-module.exports = ({github, context}) => {
-    return context.payload.client_payload.value
-}
+module.exports = async ({ github, context }) => {
+  const query = `query() {
+      repository(owner: "andriyor", name: "github-action-by-author") {
+        ref(qualifiedName: "main") {
+          target {
+            ... on Commit {
+               history(first: 1, path: "packages/qr/locales/en-US.json") {
+            edges {
+              node {
+                author {
+                  user {
+                    login
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+            }`;
+  const result = await github.graphql(query);
+    console.log(result);
+    return context.payload.client_payload;
+};
