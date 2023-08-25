b8b0ba35ed9c1758f8f363ad9f163288ca3920dd
Andrii Oriekhov <andriyorehov@gmail.com>
2023-08-25 13:52:35 +0300

    make changes to two packages

--- .github/scripts/create-bump-pr.sh
+++ .github/scripts/create-bump-pr.sh
@@ -1,11 +1,11 @@
 read package_name
 cd "packages/${package_name}"
-git checkout -b "${package_name}-bump"
+BRANCH_NAME="chore/${package_name}-bump-golf"
+git checkout -b "$BRANCH_NAME"
 npm version patch
 git config --global user.name 'Your Name'
 git config --global user.email 'andriyorehov@gmail.com'
 git add .
 git commit -m 'bump version'
-git push origin "${package_name}-bump"
-gh pr create --title "Pull request title" --body "Pull request body"
-
+git push origin "$BRANCH_NAME"
+gh pr create --title "Update ${package_name} package after sync" --body "Pull request body"
