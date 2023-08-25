7590e0ae6630dfdc4d65c021844a122e82b27784
Andrii Oriekhov <andriyorehov@gmail.com>
2023-08-25 13:11:47 +0300

    try

--- .github/scripts/main.sh
+++ .github/scripts/main.sh
@@ -1,11 +1,5 @@
 OUTPUT=$(git show --name-only --numstat --format= | ./.github/scripts/changed-packages.sh)
 
-echo 'git show --name-only --numstat --format='
-git show --name-only --numstat --format=
-
-echo 'git diff-tree --no-commit-id --name-only -r HEAD'
-git diff-tree --no-commit-id --name-only -r HEAD
-
 for key in $OUTPUT
 do
   echo "make pr for ${key}"
