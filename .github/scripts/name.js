c3f7ddd382aac4fc8962e87afd98dd0e9282c27f
Andrii Oriekhov <andriyorehov@gmail.com>
2023-08-25 15:59:06 +0300

    add script

--- .github/scripts/name.js
+++ .github/scripts/name.js
@@ -1,7 +1,29 @@
-const { exec, execSync } = require('node:child_process');
+const path = require('node:path');
+const util = require('node:util');
+const exec = util.promisify(require('node:child_process').exec);
 
 module.exports = async ({ github, context }) => {
-  console.log(execSync('git diff-tree --no-commit-id --name-only -r HEAD'));
+  const { stdout } = await exec('git diff-tree --no-commit-id --name-only -r HEAD');
+  const regex = new RegExp("packages/([^/]+)/locales");
+  const packages = new Set();
+  for (const file of [...stdout.split('\n'), 'packages/qr/locales/en-US.json']) {
+    if (regex.test(file)) {
+      const packageName = regex.exec(file)[1]
+      packages.add(packageName);
+    }
+  }
+
+  for (const packageName of [...packages]) {
+    const branchName = `chore/${packageName}-bump-golf`
+    await exec(`git checkout -b ${branchName}`);
+    await exec(`npm version patch`, {cwd: path.join(process.cwd(), 'packages', packageName) });
+    await exec("git config --global user.name 'Your Name'");
+    await exec("git config --global user.email 'andriyorehov@gmail.com'");
+    await exec("git add .");
+    await exec("git commit -m 'bump version'");
+    await exec(`git push origin ${branchName}`);
+  }
+
   const query = `{
     repository(owner: "andriyor", name: "github-action-by-author") {
       ref(qualifiedName: "main") {
--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const octokit = require("@octokit/graphql");
+const {execSync} = require("node:child_process");
+const util = require('node:util');
+const path = require('node:path');
+const exec = util.promisify(require('node:child_process').exec);
 
 // const result = octokit.graphql(
 //     `
@@ -62,38 +66,64 @@ const octokit = require("@octokit/graphql");
 
 
 
-const result = octokit.graphql(
-    `
-    {
-  repository(owner: "andriyor", name: "github-action-by-author") {
-        ref(qualifiedName: "main") {
-          target {
-            ... on Commit {
-               history(first: 1) {
-            edges {
-              node {
-                tree {
-                  id,
-                  entries {
-                    path
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
-  `,
-    {
-      headers: {
-        authorization: `token`,
-      },
-    }
-);
+// const result = octokit.graphql(
+//     `
+//     {
+//   repository(owner: "andriyor", name: "github-action-by-author") {
+//         ref(qualifiedName: "main") {
+//           target {
+//             ... on Commit {
+//                history(first: 1) {
+//             edges {
+//               node {
+//                 tree {
+//                   id,
+//                   entries {
+//                     path
+//                   }
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
+//   `,
+//     {
+//       headers: {
+//         authorization: `token`,
+//       },
+//     }
+// );
+//
+// result.then((res) => {
+//   console.log(res.repository);
+// });
 
-result.then((res) => {
-  console.log(res.repository);
-});
+
+
+
+(async () => {
+    const { stdout } = await exec('git diff-tree --no-commit-id --name-only -r HEAD');
+    const regex = new RegExp("packages/([^/]+)/locales");
+    const packages = new Set();
+    for (const file of [...stdout.split('\n'), 'packages/qr/locales/en-US.json']) {
+        if (regex.test(file)) {
+            const packageName = regex.exec(file)[1]
+            packages.add(packageName);
+        }
+    }
+
+    for (const packageName of [...packages]) {
+        const branchName = `chore/${packageName}-bump-golf`
+        await exec(`git checkout -b ${branchName}`);
+        await exec(`npm version patch`, {cwd: path.join(process.cwd(), 'packages', packageName) });
+        await exec("git config --global user.name 'Your Name'");
+        await exec("git config --global user.email 'andriyorehov@gmail.com'");
+        await exec("git add .");
+        await exec("git commit -m 'bump version'");
+        await exec(`git push origin ${branchName}`);
+    }
+})()
